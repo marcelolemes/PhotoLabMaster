a095fe823c3cba2283a31cb4025051d272d8914f
Marcelo Lemes <lemmarcelo@gmail.com>
2014-09-24 07:38:41 -0300

    24-08 alterações IDEA

--- TCC/src/br/com/login/Dao/ContratoDao.java
+++ TCC/src/br/com/login/Dao/ContratoDao.java
@@ -1,70 +1,67 @@
 package br.com.login.Dao;
 
-import java.io.Serializable;
-import java.util.List;
-
-import javax.faces.bean.ViewScoped;
-
-import org.hibernate.Criteria;
-import org.hibernate.Session;
-
 import br.com.login.model.Contrato;
 import br.com.login.util.HibernateUtil;
-
+import org.hibernate.Criteria;
+import org.hibernate.Session;
 import org.hibernate.criterion.MatchMode;
 import org.hibernate.criterion.Restrictions;
+
+import javax.faces.bean.ViewScoped;
+import java.io.Serializable;
+import java.util.List;
 
 @ViewScoped
 public class ContratoDao implements Serializable {
 
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 1L;
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1L;
 
-	public boolean Gravar(Contrato contrato) throws Exception {
-		Session sessao = HibernateUtil.getSession();
-		org.hibernate.Transaction transacao = sessao.beginTransaction();
-		sessao.saveOrUpdate(contrato);
-		transacao.commit();
-		sessao.close();
-		return true;
+    public boolean Gravar(Contrato contrato) throws Exception {
+        Session sessao = HibernateUtil.getSession();
+        org.hibernate.Transaction transacao = sessao.beginTransaction();
+        sessao.saveOrUpdate(contrato);
+        transacao.commit();
+        sessao.close();
+        return true;
 
-	}
+    }
 
-	public boolean Update(Contrato contrato) throws Exception {
-		Session sessao = HibernateUtil.getSession();
-		org.hibernate.Transaction transacao = sessao.beginTransaction();
-		try {
-			sessao.update(contrato);
-			transacao.commit();
-			sessao.close();
-			return true;
-		} catch (Exception ex) {
-			sessao.close();
-			return false;
-		}
+    public boolean Update(Contrato contrato) throws Exception {
+        Session sessao = HibernateUtil.getSession();
+        org.hibernate.Transaction transacao = sessao.beginTransaction();
+        try {
+            sessao.update(contrato);
+            transacao.commit();
+            sessao.close();
+            return true;
+        } catch (Exception e) {
+            sessao.close();
+            return false;
+        }
 
-	}
+    }
 
-	public List<Contrato> listarContratos() throws Exception {
-		Session sessao = HibernateUtil.getSession();
-		Criteria criteria = sessao.createCriteria(Contrato.class);
-		List<Contrato> listaRetorno = criteria.list();
-		sessao.close();
-		return listaRetorno;
+    public List<Contrato> listarContratos() throws Exception {
+        Session sessao = HibernateUtil.getSession();
+        Criteria criteria = sessao.createCriteria(Contrato.class);
+        List<Contrato> listaRetorno = criteria.list();
+        sessao.close();
+        return listaRetorno;
 
-	}
+    }
 
-	public Contrato pesquisarContrato(String numeroContrato) throws Exception {
-		Session sessao = HibernateUtil.getSession();
-		Criteria criteria = sessao.createCriteria(Contrato.class);
-		criteria.add(Restrictions.like("numeroContrato", numeroContrato,
-				MatchMode.ANYWHERE));
-		Contrato Retorno = (Contrato) criteria.uniqueResult();
-		sessao.close();
-		return Retorno;
+    public Contrato pesquisarContrato(String numeroContrato) throws Exception {
+        Session sessao = HibernateUtil.getSession();
+        Criteria criteria = sessao.createCriteria(Contrato.class);
+        criteria.add(Restrictions.like("numeroContrato", numeroContrato,
+                MatchMode.ANYWHERE));
+        Contrato Retorno = (Contrato) criteria.uniqueResult();
+        sessao.close();
+        return Retorno;
 
-	}
+    }
 
 }
