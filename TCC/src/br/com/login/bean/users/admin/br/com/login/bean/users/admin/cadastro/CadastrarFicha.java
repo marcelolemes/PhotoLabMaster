2192800820d6042ea58ba92d26662a6113e0d1d7
Marcelo Lemes <lemmarcelo@gmail.com>
2014-09-26 21:17:15 -0300

    casa

---
+++ TCC/src/br/com/login/bean/users/admin/br/com/login/bean/users/admin/cadastro/CadastrarFicha.java
@@ -0,0 +1,36 @@
+package br.com.login.bean.users.admin.br.com.login.bean.users.admin.cadastro;
+
+import br.com.login.Dao.FichaDao;
+import br.com.login.model.Ficha;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ViewScoped;
+import java.io.Serializable;
+
+@ManagedBean
+@ViewScoped
+public class CadastrarFicha implements Serializable {
+
+    FichaDao fichaDao;
+    private Ficha ficha = new Ficha();
+    public CadastrarFicha() {
+        fichaDao = new FichaDao();
+    }
+
+    public Ficha getFicha() {
+        return ficha;
+    }
+
+    public void setFicha(Ficha ficha) {
+        this.ficha = ficha;
+    }
+
+
+
+    public void salvarFicha() throws Exception {
+        fichaDao.Gravar(ficha);
+
+    }
+
+
+}
