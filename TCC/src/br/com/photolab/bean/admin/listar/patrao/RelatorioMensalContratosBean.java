419fc28401867aef9472b987401a3e5b35a6b448
marcelolemes <lemmarcelo@gmail.com>
2014-11-15 00:22:05 -0200

    Relatório de contratos encerrados,com exportação pra pdf

--- TCC/src/br/com/photolab/bean/admin/listar/patrao/RelatorioMensalContratosBean.java
+++ TCC/src/br/com/photolab/bean/admin/listar/patrao/RelatorioMensalContratosBean.java
@@ -170,12 +170,15 @@ public class RelatorioMensalContratosBean
     }
     public void preProcessPDF(Object document) throws Exception {
         Document pdf = (Document) document;
+        pdf.setPageSize(PageSize.A4.rotate());
         pdf.open();
         pdf.add(new Paragraph("Mês " + usuarioBean.getMesSelecionado().getNome()));
         pdf.add(new Paragraph("quantidade de Álbuns produzidas "+qtdAlbunsMes()));
         pdf.add(new Paragraph("quantidade de fotos produzidas "+qtdFotosMes()));
         pdf.add(new Paragraph(" "));
         pdf.add(new Paragraph(" "));
+        pdf.addAuthor("PhotoLabMaster");
+
     }
 
     public UsuarioBean getUsuarioBean() {
